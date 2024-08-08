37685834c8e00e2e8ce95dbb2bee5d764881a76d
AisuruCode <marcashleycrisostomo53@gmail.com>
2024-08-09 00:27:42 +0800

    fix error pop up

--- Register/index.php
+++ Register/index.php
@@ -53,7 +53,7 @@ mysqli_close($conn);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="log.css">
 </head>
 <body>
   <div class="container">
